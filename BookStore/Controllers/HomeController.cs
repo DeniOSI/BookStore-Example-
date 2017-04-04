a04a4b140762f6d1e10ebd094e33208751442a03
DeniOSI <geksabyte@gmail.com>
2017-04-04 14:31:33 +0300

    controllers

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStore.Models;
+using BookStore.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +15,7 @@ namespace BookStore.Controllers
         {
             IEnumerable<Book> books = db.Books;
             ViewBag.Books = books;
-            return View();
+            return View("Index");
         }
 
         [HttpGet]
@@ -30,6 +31,29 @@ namespace BookStore.Controllers
             db.Purchases.Add(purchase);
             db.SaveChanges();
             return "Thank you, " + purchase.Person + ", for the buy!";
+        }
+        [HttpPost]
+        public string PostBook()
+        {
+            
+            return string.Format(Request.Params["title"] + " " + Request.Params["author"]);
+         }
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+
+        public ActionResult GetHtml()
+        {
+            return new HtmlResult("<h2> hello world</h2>");
+        }
+
+        public string Square()
+        {
+            int a = int.Parse(Request.Params["a"]);
+            int b = int.Parse(Request.Params["b"]);
+            return  String.Format("<h2> Squeare triangle = " +  a * b / 2);
         }
       
     }
---
+++ BookStore/Util/HtmlResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Util
+{
+    public class HtmlResult : ActionResult
+    {
+        private string HtmlCode;
+        public HtmlResult(string html)
+        {
+            HtmlCode = html;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            string pageCode = "<!Doctype html> <html> <head> <title>";
+            pageCode += "Main page </title>";
+            pageCode += "<body> " + HtmlCode;
+            pageCode += "</body> </html>";
+            context.HttpContext.Response.Write(pageCode);
+        }
+    }
+}
